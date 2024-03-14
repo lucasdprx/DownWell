7d2fc05175f8a1248f31fa66e4fe9b1818455497
lucasdprx <l.darpeix@lahorde.school>
2024-03-14 14:44:22 +0100

    Generation procedural

--- My project/Assets/Script/ColisionShoot.cs
+++ My project/Assets/Script/ColisionShoot.cs
@@ -4,7 +4,7 @@ public class ColisionShoot : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.TryGetComponent(out InputPlayer Player))
+        if (!collision.TryGetComponent(out InputPlayer Player) && collision.gameObject.layer != 6)
         {
             Destroy(collision.gameObject);
             Destroy(gameObject);
--- My project/Assets/Script/GenerationProcedural.cs
+++ My project/Assets/Script/GenerationProcedural.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class GenerationProcedural : MonoBehaviour
@@ -7,81 +5,30 @@ public class GenerationProcedural : MonoBehaviour
     [SerializeField] private GameObject _blockDestructible;
     [SerializeField] private GameObject _blockInvinsible;
 
-    [SerializeField] private int _widht;
-    [SerializeField] private int _height;
+    [SerializeField] private Transform _player;
 
-    private List<List<int>> _blocks = new();
+    [SerializeField] private int _seed;
+
+    [SerializeField] private float _chanceSpawn;
+    [SerializeField] private int _height;
+    [SerializeField] private int _width;
+
     void Start()
     {
-        for (int i = 0; i < _widht; i++)
-        {
-            _blocks.Add(new List<int>(_height));
-            for (int j = 0; j < _height; j++)
-            {
-                _blocks[i].Add(0);
-            }
-        }
-        GenerationMapLeft();
-        GenerationMapRight();
-        GenerateBlockDestructible();
+        _seed = Random.Range(-100000, 100000);
+        GenerationMap();
     }
 
-    private void GenerationMapLeft()
+    public void GenerationMap()
     {
-        int _rand = Random.Range(0, _height / 2);
-        for (int i  = 0; i < _widht; i++)
+        for (int y = (int)_player.position.y - 2; y > -_height; y--)
         {
-            for (int j = 0; j < _rand; j++)
+            for (int x = 0; x < _width; x++)
             {
-                GameObject tile = Instantiate(_blockDestructible, new Vector3(j, i), Quaternion.identity);
-                _blocks[i][j] = 1;
-            }
-            _rand += Random.Range(-2, 3);
-            if (_rand > (_height - 1) / 2)
-                _rand = (_height - 1) / 2;
-            if (_rand < 0)
-                _rand = 0;
-            Instantiate(_blockDestructible, new Vector3(0, i), Quaternion.identity);
-            _blocks[i][0] = 1;
-        }
-    }
-    private void GenerationMapRight()
-    {
-        int _rand = Random.Range(0, _height / 2);
-        for (int i = 0; i < _widht; i++)
-        {
-            for (int j = 0; j < _rand; j++)
-            {
-                GameObject tile = Instantiate(_blockDestructible, new Vector3(_height - 1 - j, i), Quaternion.identity);
-                _blocks[i][_height - 1 - j] = 1;
-            }
-            _rand += Random.Range(-2, 3);
-            if (_rand > (_height - 1) / 2)
-                _rand = (_height - 1) / 2;
-            if (_rand < 0)
-                _rand = 0;
-            Instantiate(_blockDestructible, new Vector3(_height - 1, i), Quaternion.identity);
-            _blocks[i][_height - 1] = 1;
-        }
-    }
-
-    private void GenerateBlockDestructible()
-    {
-        for (int i = 0; i < _widht; i++)
-        {
-            for (int j = 0; j < _height; j++)
-            {
-                if (Random.Range(0,8) == 0)
-                    if (_blocks[i][j] == 0 && _blocks[i][j + 1] == 0)
-                    {
-                        Instantiate(_blockInvinsible, new Vector3(j, i), Quaternion.identity);
-                        Instantiate(_blockInvinsible, new Vector3(j + 1, i), Quaternion.identity);
-                    }
-                else if (Random.Range(0, 8) == 1)
-                        for (int k = 0; k < 3; k++)
-                        {
-
-                        }
+                if (Mathf.PerlinNoise(x / 10f + _seed, y / 10f + _seed) >= _chanceSpawn)
+                {
+                    Instantiate(_blockDestructible, new Vector3(x, y), Quaternion.identity, gameObject.transform);
+                }
             }
         }
     }
